42d266216b1fd487b5cdd7ff85e7938446eef147
Samarpan Rai <106384625+samarpan-rai11@users.noreply.github.com>
2023-11-04 23:42:11 +0545

    Update oop_project.py

--- OOP-mini-Project/oop_project.py
+++ OOP-mini-Project/oop_project.py
@@ -3,13 +3,13 @@ from bank_accounts import *
 Samarpan = BankAccount(1000,"Samarpan")
 Nepal = BankAccount(10000,"Nepal")
 
-# Samarpan.get_balance()
+Samarpan.get_balance()
 
-# Samarpan.deposit(2000)
+Samarpan.deposit(2000)
 
-# Nepal.withdraw(1000)
+Nepal.withdraw(1000)
 
-# Nepal.transfer(10,Samarpan)
+Nepal.transfer(10,Samarpan)
 
 Jim = InterestReward(1000,"Jim")
 Jim.get_balance()
